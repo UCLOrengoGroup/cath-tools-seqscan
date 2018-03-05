05ae03417a5efa3fccafa44b7ce8b869fc64cf9f
sillitoe <ian@sillit.com>
2018-03-05 18:51:37 +0000

    update scripts

---
+++ script/cath-tools-api-funfam.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use lib "$FindBin::Bin/../extlib/lib/perl5";
+
+use Log::Any::Adapter ('Stdout');
+
+use Cath::Tools::Api::Funfam;
+
+my $app = Cath::Tools::Api::Funfam->new_with_options();
+
+$app->run;
