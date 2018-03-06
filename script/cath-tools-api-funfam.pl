bafbd719ccd7ce197421ea78ec40bd93af22bf1d
sillitoe <ian@sillit.com>
2018-03-06 11:13:25 +0000

    add arch specific library for deps

--- script/cath-tools-api-funfam.pl
+++ script/cath-tools-api-funfam.pl
@@ -3,8 +3,10 @@
 use strict;
 use warnings;
 use FindBin;
+use Config;
 use lib "$FindBin::Bin/../lib";
 use lib "$FindBin::Bin/../extlib/lib/perl5";
+use lib "$FindBin::Bin/../extlib/lib/perl5/$Config{archname}";
 
 use Log::Any::Adapter ('Stdout');
 
